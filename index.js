581bd0b09e5f2d848f69e1429e53d4899d2d4fc7
wijeremy <jeremydavwilliams@gmail.com>
2021-07-26 13:10:45 -0500

    okay, it finally works. i really don't know what is different other than i just took my time and went through feature by feature and kept adding from test to make sure each feature worked before i added then next. i can finally move on to doing something with the objects returned from the user interface.'

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 const inquirer = require("inquirer");
+const { Manager } = require("./constructor.js");
 const employee = require('./constructor.js')
-const prompt = require('./prompts')
+const promptsFile = require('./prompts')
+const { base, managerPrompt, engineerPrompt, internPrompt, select } = promptsFile
 
-const collectInputs = async (inputs = [], prompt) => {
-    const selectEmployee = async (inputs = []) => {
-        console.log(`So far we have ${inputs}`)
-        const { role } = await inquirer.prompt(select);
-        console.log(role)
-        if (role === "Engineer"){
-            console.log('So you want to add an engineer?')
-            collectInputs(inputs, engineerPrompt)
-        } else if (role === "Intern"){
-            console.log('So you want to add an intern?')
-            collectInputs(inputs, internPrompt)
+const collectInputs = async (inputs = [], rolePrompt) => {
+    const getPrompt = (promptName) => {
+        if (!promptName) {
+            return managerPrompt;
         } else {
-            console.log("here you goooooo")
-            return inputs;
+            return rolePrompt;
         }
     }
-    console.log("Let's start with the manager:")
-    let newPrompts = [...prompts, prompt, confirm]
-    let { again, ...answers } = await inquirer.prompt(newPrompts);
-    if (orompt === managerPrompt) {
-        let manager = new employee.Manager(answers.name, answers.id, answers.email, answers.phone)
-        let managerAdded = [...inputs, manager];
-        return again ? selectEmployee(managerAdded) : managerAdded;
-    } else if (prompt === engineerPrompt) {
-        let engineer = new employee.Engineer(answers.name, answers.id, answers.email, answers.github);
-        let engineerAdded = [...inputs, engineer];
-        return again ? selectEmployee(engineerAdded) : engineerAdded;
-    } else if (prompt === internPrompt) {
-        let intern = new employee.Intern(answers.name, answers.id, answers.email, answers.school);
-        let internAdded = [...inputs, intern];
-        return again ? selectEmployee(internAdded) : internAdded;
-    } else {
-        console.log("here you go:")
-        return inputs
-    }  
-    
+    const myPrompt = getPrompt(rolePrompt);
+    const myPrompts = [...base, myPrompt, select];
+    const { nextRole, ...answers } = await inquirer.prompt(myPrompts);
+    const setRole = (currentPrompt) => {
+        if (currentPrompt === managerPrompt){
+            return new employee.Manager(...Object.values(answers))
+        } else if (currentPrompt === engineerPrompt){
+            return new employee.Engineer(...Object.values(answers))
+        } else if (currentPrompt === internPrompt){
+            return new employee.Intern(...Object.values(answers))
+        } else {
+            return answers
+        }
+    }
+    const newEmployee = setRole(myPrompt)
+    const newInputs = [...inputs, newEmployee];
+    const getNextPrompt = (roleName) => {
+        if (roleName === 'Engineer'){
+            return engineerPrompt
+        } else if (roleName === 'Intern') {
+            return internPrompt
+        } else {
+            return false
+        }
+    };
+    const nextPrompt = getNextPrompt(nextRole);
+    return nextPrompt ? collectInputs(newInputs, nextPrompt) : newInputs;
 };
 
-
   
-  const main = async () => {
+const main = async () => {
     const inputs = await collectInputs();
-    console.log(`the final results are ${inputs}`);
-  };
+    console.log(inputs)
+};
   
-  main();
+main();
 
 
